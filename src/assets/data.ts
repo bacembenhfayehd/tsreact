ee650df3a74ca5d3d8148d1170cf0398d30957ed
bacembenhfayehd <bacem.benhfayedh@edu.isetcom.tn>
2025-04-24 09:02:23 +0100

    first

---
+++ src/assets/data.ts
@@ -0,0 +1,24 @@
+import bergerAll from '../../public/images/png-clipart-adult-german-shepherd-german-shepherd-puppy-purebred-dog-dog-food-dog-breed-dogs-animals-carnivoran-thumbnail.png'
+import bergerMal from '../../public/images/pngtree-belgian-shepherd-malinois-pet-guard-png-image_10948858.png'
+import pit from '../../public/images/049409f3772977504112645871cec159.jpg'
+import elephant from '../../public/images/pngtree-belgian-shepherd-malinois-pet-guard-png-image_10948858.png'
+import bird from '../../public/images/png-transparent-birds-birds-birds-natural-bird-thumbnail.png'
+import turkishCat from '../../public/images/ai-generated-turkish-van-cat-isolated-on-a-transparent-background-free-png.png'
+import arabeCat from '../../public/images/png-clipart-cats-cats.png'
+import lion from '../../public/images/26525162-lion-animal-isole-photo.jpg'
+import arabeHorse from '../../public/images/png-clipart-arabian-horse-appaloosa-andalusian-horse-bay-arab-horse-miscellaneous.png'
+import europenHorse from '../../public/images/horse-galloping-on-sand.jpg'
+
+
+export const animals = [
+{id:1,name:'fox',city:'tataouine',race:'allemand',image:bergerAll},
+{id:2,name:'rex',city:'gabes',race:'malinois',image:bergerMal},
+{id:3,name:'arabe horse',city:'sfax',race:'arabe',image:arabeHorse},
+{id:4,name:'europeen horse',city:'tunis',race:'europe',image:europenHorse},
+{id:5,name:'birs',city:'tataouine',race:'belge',image:bird},
+{id:6,name:'elephant',city:'tataouine',race:'afirque',image:elephant},
+{id:7,name:'pit',city:'tataouine',race:'canada',image:pit},
+{id:8,name:'turkish',city:'gafsa',race:'turkish',image:turkishCat},
+{id:9,name:'arabe',city:'gafsa',race:'arabe',image:arabeCat},
+{id:10,name:'lion',city:'senegal',race:'senegal',image:lion},
+]
